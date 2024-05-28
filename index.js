adf1c795719af02553f303ec23ec6928ed739909
Nagesh N <nagesh.n8618@gmail.com>
2024-05-28 12:43:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const showModal = document.querySelector('.show-modal');
+const modal = document.querySelector('.modal-content');
+const closeModal = document.querySelector('.close');
+
+showModal.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
